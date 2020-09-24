c3668395a8205e44fccc954da22108aa596b5470
Evan Pierce <evanpierce@aim.com>
2020-09-23 22:02:38 -0400

    cli method stubs and filepath validaton

--- src/main.go
+++ src/main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 	serverPtr := flag.Bool("server", false, "Starts executable in server mode")
+	civPathPtr := flag.String("dir", DefaultCivDirectory, "Specifiy nonstandard civ folder (default is: "+DefaultCivDirectory+")")
 	flag.Parse()
 
 	if *serverPtr {
@@ -18,6 +21,46 @@ func main() {
 		StartServer()
 	}
 
+	//CLI Start
+	//Detect Civ folder (check flag then go to default )
+
+	civBasePath := filepath.Clean(*civPathPtr)
+	civDlcPath := filepath.Join(civBasePath, "Assets", "DLC")
+	civMapsPath := filepath.Join(civBasePath, "Assets", "Maps")
+	fmt.Println(civBasePath)
+
+	validateFolderExists(civDlcPath)
+	validateFolderExists(civMapsPath)
+
+	//Delete the old files if they exist (using a regex)
+	deleteFiles(filepath.Join(civMapsPath, "*[Ll]ek"))
+
+	//get new file links from server (and parse them)
+
+	//download them to cache/temp folder
+	//unzip them to civ folder
+
+}
+
+//https://civ5-nq-launcher.herokuapp.com/currentLinks
+func getFileLinks(endpoint string) (mapLink, modLink string) {
+
+}
+
+func unzip(zipPath, extractToPath) {
+
+}
+
+func deleteFiles(glob string) {
+
+}
+
+func validateFolderExists(path string) {
+	if val, err := os.Stat(path); os.IsNotExist(err) {
+		panic("Civ5 folder not found @ " + path)
+	} else if !val.IsDir() {
+		panic("Found file where expected folder, why the heck is a file @ " + path)
+	}
 }
 
 func getFile() {
@@ -36,7 +79,7 @@ func getFile() {
 
 	// Find and print image URLs
 	document.Find("a#uc-download-link").Each(func(index int, element *goquery.Selection) {
-		href, exists := element.Attr("href")
+		href, exists := element.Attr("href")``
 		if exists {
 			link := "https://drive.google.com/u/0" + href
 			DownloadFile("zipfile.zip", link)
