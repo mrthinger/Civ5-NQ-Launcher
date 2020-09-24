7504a0159a127c3ca1b0a2726486e506b2c18183
Evan Pierce <evanpierce@aim.com>
2020-09-23 21:01:46 -0400

    update current links

--- src/server.go
+++ src/server.go
@@ -5,6 +5,7 @@ import (
 )
 
 const (
+	//DefaultPort for server if "PORT" env variable is not specified
 	DefaultPort = "8080"
 )
 
@@ -12,9 +13,10 @@ const (
 func StartServer() {
 	r := gin.Default()
 	port := GetEnv("PORT", DefaultPort)
-	r.GET("/currentLek", func(c *gin.Context) {
+	r.GET("/currentLinks", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"currentLink": "https://",
+			"mod": "https://storage.googleapis.com/civ5-mods/lek-mod/LEKMOD_V25.1.zip",
+			"map": "https://storage.googleapis.com/civ5-mods/lek-map/Lekmap%20v1.4.2.zip",
 		})
 	})
 	r.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
