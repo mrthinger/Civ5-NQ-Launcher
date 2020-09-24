fcb6cd45be2221a7aa7816e7915bb67bfea716b3
Evan Pierce <evanpierce@aim.com>
2020-09-23 22:03:52 -0400

    add config file

---
+++ src/config.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	//DefaultCurrentLinksEndpoint hosted endpoint for current links
+	DefaultCurrentLinksEndpoint = "https://civ5-nq-launcher.herokuapp.com/currentLinks"
+
+	//DefaultCivDirectory default installfolder if none was provided
+	DefaultCivDirectory = "C:/Program Files (x86)/Steam/steamapps/common/Sid Meier's Civilization Vs"
+)
