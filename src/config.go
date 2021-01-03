44ef6231719decadc35a6843990f8a4dc4c3f2da
Evan Pierce <evanpierce@aim.com>
2021-01-02 19:43:42 -0500

    increment build number

--- src/config.go
+++ src/config.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	//CLIBuildNumber increment to force update
-	CLIBuildNumber = 4
+	CLIBuildNumber = 5
 
 	//DefaultCurrentLinksEndpoint hosted endpoint for current links
 	DefaultCurrentLinksEndpoint = "https://civ5-nq-launcher.herokuapp.com/currentLinks"
